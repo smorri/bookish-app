75d4d7614008766bd60b67f8bd4500df72edf0ef
Samone <samonemorris@gmail.com>
2019-01-21 11:54:13 -0600

    Removed an unexpected token from the index file

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 5000;
 var application = express();
 
 application
-	.get('/', function( req, res ){ res.send( JSON.stringify({ Hello: 'World'}); ); });
-	.listen( PORT, function (){ console.log( 'Listening on $( PORT )' ); } );
+	.get('/', function( req, res ){ res.send( JSON.stringify({ Hello: 'World'})); })
+	.listen( PORT, function (){ console.log( 'Listening on $( PORT )' ); } )
