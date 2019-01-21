9e3ed045e68199dde03479d8bf6507ca8036870c
Samone <samonemorris@gmail.com>
2019-01-21 13:53:21 -0600

    Added 'use' line to index.js. Also, forgot to merge in files in ejs directory

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 var application = express();
 
 application
+	.use( express.static( path.join(__dirname, 'public') ) )
 	.set( 'view-engine', 'ejs' )
 	.get( '/', function( req, res ){ 
 			res.render( 'pages/index' );
